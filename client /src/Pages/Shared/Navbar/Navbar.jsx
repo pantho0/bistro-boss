728340ad94e991c080677d2cee06890dd8f777fc
pantho0 <panthomashreky6@gmail.com>
2024-01-16 18:52:59 +0600

    search button added in nav

--- client /src/Pages/Shared/Navbar/Navbar.jsx
+++ client /src/Pages/Shared/Navbar/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
           <ul className="menu menu-horizontal px-1">{navOptions}</ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <a className="btn">Search</a>
         </div>
       </div>
     </>
