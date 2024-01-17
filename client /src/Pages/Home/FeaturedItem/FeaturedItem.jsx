09327021278bb426c2370037b7924aeeade1c891
pantho0 <panthomashreky6@gmail.com>
2024-01-17 21:55:05 +0600

    featured food item section added

---
+++ client /src/Pages/Home/FeaturedItem/FeaturedItem.jsx
@@ -0,0 +1,22 @@
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import featuredImage from "../../../assets/home/featured.jpg"
+import './Featured.css'
+
+
+const FeaturedItem = () => {
+    return (
+        <div className="my-20 featured-item py-20 text-white opacity-95">
+            <SectionTitle subHeading={'Check It Out'} heading={'Featured Items'}/>
+            <div className="md:flex gap-16 justify-center items-center px-40">
+                <img className="w-2/4" src={featuredImage} alt="Featured Food of Bistro Boss" />
+                <div>
+                    <p>11 July, 1995</p>
+                    <h6>Where Can I Get Some?</h6>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae optio, aspernatur voluptate neque nemo saepe quia possimus, explicabo quaerat hic asperiores. Temporibus excepturi vitae quia dignissimos accusamus culpa mollitia ratione aspernatur ad quasi. Deleniti optio omnis nesciunt ratione dolorum, nemo, ipsa nostrum illum esse, facere id a corporis. Accusamus, incidunt.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FeaturedItem;
--- client /src/Pages/Home/Home/Home.jsx
+++ client /src/Pages/Home/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
+import FeaturedItem from "../FeaturedItem/FeaturedItem";
 import PopularMenu from "../PopularMenu/PopularMenu";
 
 
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner/>
             <Category/>
             <PopularMenu/>
+            <FeaturedItem/>
         </div>
     );
 };
