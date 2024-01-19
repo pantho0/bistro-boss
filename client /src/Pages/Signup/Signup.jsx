e490d4f3830ffc3ef5ae11b4dfda3ec33c605614
pantho0 <panthomashreky6@gmail.com>
2024-01-19 20:51:10 +0600

    signup page added, login & signup & logout function added to context api

--- client /src/Pages/Signup/Signup.jsx
+++ client /src/Pages/Signup/Signup.jsx
@@ -3,30 +3,16 @@ import { loadCaptchaEnginge, validateCaptcha, LoadCanvasTemplateNoReload } from 
 import { AuthContext } from '../../Provider/AuthProvider';
 import { Link } from 'react-router-dom';
 
-const Login = () => {
+const Signup = () => {
     const {user, loading} = useContext(AuthContext)
     
-    const [disabled, setDisabled] = useState(true)
-    useEffect(()=>{
-      loadCaptchaEnginge(6)
-    },[])
-    const captchaTXT = useRef(null)
+   
 
-
-    const handleValidate = () =>{
-      const user_captcha_value = captchaTXT.current.value;
-      console.log(user_captcha_value);
-      if (validateCaptcha(user_captcha_value)==true) {
-        setDisabled(false)
-    }
-    else {
-        setDisabled(true)
-    }
-    }
-
-    const handleLogin = e =>{
+    const handleSignup = e =>{
         e.preventDefault()
         const form = e.target;
+        const name = form.name.value;
+        const image = form.image.value;
         const email = form.email.value;
         const password = form.password.value;
         console.log(email, password);
@@ -35,7 +21,7 @@ const Login = () => {
     <div className="hero min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
+          <h1 className="text-5xl font-bold">Signup now!</h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
@@ -43,7 +29,31 @@ const Login = () => {
           </p>
         </div>
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form onSubmit={handleLogin} className="card-body">
+          <form onSubmit={handleSignup} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter Your Name Here"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Picture</span>
+              </label>
+              <input
+                type="text"
+                name="picture"
+                placeholder="upload your picture"
+                className="input input-bordered"
+                required
+              />
+            </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -68,26 +78,11 @@ const Login = () => {
                 required
               />
             </div>
-            <div className="form-control">
-              <label className="label">
-              <LoadCanvasTemplateNoReload/>
-              </label>
-              <div>
-              </div>
-              <input
-                type="text"
-                name="captcha"
-                onBlur={handleValidate} 
-                ref={captchaTXT}
-                placeholder="Type the captcha above"
-                className="input input-bordered"
-                required
-              />
-            </div>
+
             <div className="form-control mt-6">
-              <input disabled={disabled} className="btn btn-primary" type="submit" value="Login" />
+              <input className="btn btn-primary" type="submit" value="Signup" />
             </div>
-            <p className='text-center'><small>Don&apos;t Have an account <Link to={'/signup'} className='text-orange-600 cursor-pointer font-bold'>Sign Up Here</Link></small></p>
+            <p className='text-center'><small>Already have an account <Link to={'/login'} className='text-orange-600 cursor-pointer font-bold'>Login Here</Link></small></p>
           </form>
         </div>
       </div>
@@ -95,4 +90,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Signup;
--- client /src/Provider/AuthProvider.jsx
+++ client /src/Provider/AuthProvider.jsx
@@ -1,27 +1,56 @@
-import { createContext, useState } from "react";
-import { getAuth } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import app from "../Firebase/firebase.config";
+import { GoogleAuthProvider } from "firebase/auth";
 
 
 
 export const AuthContext = createContext(null)
 const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
 const AuthProvider = ({children}) => {
         const [user, setUser] = useState([])
         const [loading, setLoading] = useState(true)
 
+        const createUser = (email, password) =>{
+            return createUserWithEmailAndPassword(auth, email, password)
+
+        }
+
+        const signIn = (email, password) =>{
+            return signInWithEmailAndPassword(auth, email, password)
+        }
+
+        const googleLogin = () =>{
+            return signInWithPopup(auth, provider)
+        }
+
+        const logOut = () =>{
+            return signOut(auth)
+        }
+ 
+
+        useEffect(()=>{
+            const unsubscribe = onAuthStateChanged(auth, currentUser=>{
+                if(currentUser){
+                    console.log('Current Logged User', currentUser)
+                    setLoading(false)
+                }
+            })
+            return () =>{
+                return unsubscribe()
+            }
+        },[])
 
         const info ={
             user,
-            loading
+            loading,
+            createUser,
+            signIn,
+            logOut,
+            googleLogin
         }
-
-
-
-
-
-
 
     return (
         <AuthContext.Provider value={info}>
