f3e356abed889e5586a60edbb530c82168e29272
pantho0 <panthomashreky6@gmail.com>
2024-01-28 20:38:18 +0600

    stripe installed and primary configuration added

---
+++ client /src/Pages/Dashboard/Payment/CheckOutForm.jsx
@@ -0,0 +1,43 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+
+const CheckOutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+    if (card == null) {
+      return;
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement
+        options={{
+          style: {
+            base: {
+              fontSize: "16px",
+              color: "#424770",
+              "::placeholder": {
+                color: "#aab7c4",
+              },
+            },
+            invalid: {
+              color: "#9e2146",
+            },
+          },
+        }}
+      />
+      <button className="btn btn-sm bg-primary text-white mt-3" type="submit" disabled={!stripe}>
+        Pay
+      </button>
+    </form>
+  );
+};
+
+export default CheckOutForm;
--- client /src/Pages/Dashboard/Payment/Payment.jsx
+++ client /src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,9 +1,23 @@
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 
+
+import {loadStripe} from '@stripe/stripe-js';
+import {Elements} from "@stripe/react-stripe-js"
+import CheckOutForm from "./CheckOutForm";
+
+
+//todo: add publishable key
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY)
+
 const Payment = () => {
     return (
         <div>
             <SectionTitle heading="Payment" subHeading="Please Pay First To Eat" />
+            <div>
+                <Elements stripe={stripePromise}>
+                    <CheckOutForm/>
+                </Elements>
+            </div>
         </div>
     );
 };
